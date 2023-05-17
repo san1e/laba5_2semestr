b4b5cee2c7ca89498a39e7a946efb4a68c22a7e6
sashaklymenko <alklimenko227@outlook.com>
2023-05-17 10:16:15 +0300

    3

---
+++ Laba5_2semestr/Shab/Shab.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba5_2semestr.Shab
+{
+    public partial class Shab
+    {
+        public void Main()
+        {
+
+        }
+    }
+}
